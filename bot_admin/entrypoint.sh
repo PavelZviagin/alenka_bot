8dacbef756797123269fa824b25c1d1e438b0ac8
pazvyagin <pazvyagin@avito.ru>
2024-07-19 00:06:55 +0300

    Fix static

--- bot_admin/entrypoint.sh
+++ bot_admin/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python manage.py migrate && python manage.py runserver 0.0.0.0:80
+python manage.py migrate && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:80
